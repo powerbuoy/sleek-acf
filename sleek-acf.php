49b8932bed6a722d7b35fda3659dc237ded042d3
powerbuoy <powerbuoy@gmail.com>
2020-02-20 14:34:51 +0100

    Redirect URL support

--- sleek-acf.php
+++ sleek-acf.php
@@ -143,3 +143,52 @@ add_filter('acf/location/rule_match/nav_menu_item_depth', function ($match, $rul
 
 	return $match;
 }, 10, 4);
+
+##############
+# Redirect URL
+# TODO: Move to sleek-acf under theme-support sleek-acf-redirect-url
+add_action('after_setup_theme', function () {
+	if (get_theme_support('sleek-acf-redirect-url')) {
+		# Make sure the_permalink() points to the redirect URL
+		add_filter('the_permalink', function ($url, $postId) {
+			if (function_exists('get_field')) {
+				$redirectUrl = get_field('redirect_url', $postId);
+
+				if (!empty($redirectUrl)) {
+					return $redirectUrl;
+				}
+			}
+
+			return $url;
+		}, 10, 2);
+
+		# Redirect single pages to the redirect URL
+		add_action('template_redirect', function () {
+			if (is_singular() and function_exists('get_field')) {
+				global $post;
+
+				$redirectUrl = get_field('redirect_url', $post->ID);
+
+				if (!empty($redirectUrl)) {
+					wp_redirect($redirectUrl);
+				}
+			}
+		}, 10, 1);
+
+		# Remove posts with redirect_url from sitemap
+		add_filter('wpseo_exclude_from_sitemap_by_post_ids', function () {
+			return get_posts([
+				'post_type' => 'any',
+				'posts_per_page' => -1,
+				'fields' => 'ids',
+				'meta_query' => [
+					[
+						'key' => 'redirect_url',
+						'compare' => '!=',
+						'value' => ''
+					]
+				]
+			]);
+		});
+	}
+});
