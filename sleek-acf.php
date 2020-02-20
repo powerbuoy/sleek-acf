7f0c2acded6191915d6329b0e718f7aaa5e0e072
powerbuoy <powerbuoy@gmail.com>
2020-02-20 14:51:48 +0100

    Prevent warnings

--- sleek-acf.php
+++ sleek-acf.php
@@ -122,26 +122,30 @@ add_action('admin_head', function () {
 ###########################################
 # Add nav_menu_item_depth location ♥️ Simon
 add_filter('acf/location/rule_match/nav_menu_item_depth', function ($match, $rule, $options, $field_group) {
-	if ($rule['operator'] === '==') {
-		$match = ($options['nav_menu_item_depth'] == $rule['value']);
-	}
-	elseif ($rule['operator'] === '!=') {
-		$match = ($options['nav_menu_item_depth'] != $rule['value']);
-	}
-	elseif ($rule['operator'] === '>') {
-		$match = ($options['nav_menu_item_depth'] > $rule['value']);
-	}
-	elseif ($rule['operator'] === '<') {
-		$match = ($options['nav_menu_item_depth'] < $rule['value']);
-	}
-	elseif ($rule['operator'] === '>=') {
-		$match = ($options['nav_menu_item_depth'] >= $rule['value']);
-	}
-	elseif ($rule['operator'] === '<=') {
-		$match = ($options['nav_menu_item_depth'] <= $rule['value']);
+	if (isset($options['nav_menu_item_depth']) and isset($rule['operator']) and isset($rule['value'])) {
+		if ($rule['operator'] === '==') {
+			$match = ($options['nav_menu_item_depth'] == $rule['value']);
+		}
+		elseif ($rule['operator'] === '!=') {
+			$match = ($options['nav_menu_item_depth'] != $rule['value']);
+		}
+		elseif ($rule['operator'] === '>') {
+			$match = ($options['nav_menu_item_depth'] > $rule['value']);
+		}
+		elseif ($rule['operator'] === '<') {
+			$match = ($options['nav_menu_item_depth'] < $rule['value']);
+		}
+		elseif ($rule['operator'] === '>=') {
+			$match = ($options['nav_menu_item_depth'] >= $rule['value']);
+		}
+		elseif ($rule['operator'] === '<=') {
+			$match = ($options['nav_menu_item_depth'] <= $rule['value']);
+		}
+
+		return $match;
 	}
 
-	return $match;
+	return false;
 }, 10, 4);
 
 ##############
